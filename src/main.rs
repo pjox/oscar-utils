3b7d1fed735a92a5ece61a2f31b9f2e97a377d8b
Pedro Ortiz Suarez <pedro.ortiz@dfki.de>
2023-05-10 17:55:09 +0200

    first prototype for selecting data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use serde_json::Value;
+use std::{
+    env,
+    fs::{self, File},
+    io::{BufRead, BufReader, BufWriter, Write},
+    path::Path,
+};
+use walkdir::{DirEntry, WalkDir};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let folder = &args[1];
+    let file_paths: Vec<walkdir::DirEntry> = WalkDir::new(folder)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|e| e.file_type().is_file())
+        .filter(|e| e.file_name().to_str().unwrap().ends_with(".zst"))
+        .collect();
+
+    let path = Path::new("clean_de.jsonl");
+    let clean_file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", path.display(), why),
+        Ok(file) => file,
+    };
+    let mut writer = BufWriter::new(clean_file);
+
+    for file in file_paths {
+        let decoder = {
+            let file = fs::File::open(file.path()).unwrap();
+            zstd::Decoder::new(file).unwrap()
+        };
+        let reader = BufReader::new(decoder);
+        for line in reader.lines() {
+            let v: Value = serde_json::from_str(&line.unwrap()).unwrap();
+            writeln!(writer, "{}\n", v["content"]).unwrap();
+        }
+    }
 }
