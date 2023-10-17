867ab216a73fb3d592160f7da80ecdbe72a5c8c0
Pedro Ortiz Suarez <pedro.ortiz@dfki.de>
2023-10-17 13:58:53 +0200

    Added counting function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use oscar_io::v3::Document;
 use std::{
     env,
-    fs::{self, File},
-    io::{BufRead, BufReader, BufWriter, Write},
-    path::Path,
+    fs::{self},
+    io::{BufRead, BufReader},
 };
 use walkdir::{DirEntry, WalkDir};
+use words_count::WordsCount;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,19 +18,12 @@ fn main() {
         .filter(|e| e.file_name().to_str().unwrap().ends_with(".zst"))
         .collect();
 
-    let path = Path::new(&args[2]);
-    let clean_file = match File::create(&path) {
-        Err(why) => panic!("couldn't create {}: {}", path.display(), why),
-        Ok(file) => file,
+    let mut total_count = WordsCount {
+        words: 0,
+        characters: 0,
+        cjk: 0,
+        whitespaces: 0,
     };
-    let mut writer = BufWriter::new(clean_file);
-
-    let mut clean_count = 0;
-    let mut tiny_count = 0;
-    let mut short_sentences_count = 0;
-    let mut header_count = 0;
-    let mut footer_count = 0;
-    let mut noisy_count = 0;
 
     for file in file_paths {
         let decoder = {
@@ -38,136 +31,18 @@ fn main() {
             zstd::Decoder::new(file).unwrap()
         };
         let reader = BufReader::new(decoder);
+        let mut file_count = WordsCount {
+            words: 0,
+            characters: 0,
+            cjk: 0,
+            whitespaces: 0,
+        };
         for line in reader.lines() {
             let doc = serde_json::from_str::<Document>(&line.unwrap()).unwrap();
-            if is_clean(&doc) && clean_count < 10 {
-                writeln!(writer, "{}\t{}\tclean", doc.warc_id(), doc.url().unwrap()).unwrap();
-                clean_count += 1;
-                continue;
-            }
-            if is_tiny(&doc) && tiny_count < 10 {
-                writeln!(writer, "{}\t{}\ttiny", doc.warc_id(), doc.url().unwrap()).unwrap();
-                tiny_count += 1;
-                continue;
-            }
-            if is_short_sentences(&doc) && short_sentences_count < 10 {
-                writeln!(
-                    writer,
-                    "{}\t{}\tshort_sentences",
-                    doc.warc_id(),
-                    doc.url().unwrap()
-                )
-                .unwrap();
-                short_sentences_count += 1;
-                continue;
-            }
-            if is_header(&doc) && header_count < 10 {
-                writeln!(writer, "{}\t{}\theader", doc.warc_id(), doc.url().unwrap()).unwrap();
-                header_count += 1;
-                continue;
-            }
-            if is_footer(&doc) && footer_count < 10 {
-                writeln!(writer, "{}\t{}\tfooter", doc.warc_id(), doc.url().unwrap()).unwrap();
-                footer_count += 1;
-                continue;
-            }
-            if is_noisy(&doc) && noisy_count < 10 {
-                writeln!(writer, "{}\t{}\tnoisy", doc.warc_id(), doc.url().unwrap()).unwrap();
-                noisy_count += 1;
-                continue;
-            }
-            if clean_count >= 10
-                && tiny_count >= 10
-                && short_sentences_count >= 10
-                && header_count >= 10
-                && footer_count >= 10
-                && noisy_count >= 10
-            {
-                break;
-            }
+            let count = words_count::count(doc.content());
+            file_count += count;
         }
+        total_count += file_count;
     }
-}
-
-fn is_tiny(doc: &Document) -> bool {
-    match doc.metadata().annotation() {
-        Some(categories) => {
-            if categories.contains(&"tiny".to_string()) {
-                return true;
-            }
-        }
-        None => return false,
-    }
-    false
-}
-
-fn is_short_sentences(doc: &Document) -> bool {
-    match doc.metadata().annotation() {
-        Some(categories) => {
-            if categories.contains(&"short_sentences".to_string()) {
-                return true;
-            }
-        }
-        None => return false,
-    }
-    false
-}
-
-fn is_header(doc: &Document) -> bool {
-    match doc.metadata().annotation() {
-        Some(categories) => {
-            if categories.contains(&"header".to_string()) {
-                return true;
-            }
-        }
-        None => return false,
-    }
-    false
-}
-
-fn is_footer(doc: &Document) -> bool {
-    match doc.metadata().annotation() {
-        Some(categories) => {
-            if categories.contains(&"footer".to_string()) {
-                return true;
-            }
-        }
-        None => return false,
-    }
-    false
-}
-
-fn is_noisy(doc: &Document) -> bool {
-    match doc.metadata().annotation() {
-        Some(categories) => {
-            if categories.contains(&"footer".to_string()) {
-                return true;
-            }
-        }
-        None => return false,
-    }
-    false
-}
-
-fn is_clean(doc: &Document) -> bool {
-    if doc.metadata().annotation().is_some() {
-        return false;
-    }
-    match doc.metadata().categories() {
-        Some(categories) => {
-            if categories.contains(&"adult".to_string())
-                || categories.contains(&"agressif".to_string())
-                || categories.contains(&"cryptojacking".to_string())
-                || categories.contains(&"malware".to_string())
-                || categories.contains(&"mixed_adult".to_string())
-            {
-                return false;
-            }
-        }
-        None => (),
-    }
-    if doc.metadata().harmful_pp() < Some(8.0) {
-        return false;
-    }
-    true
+    println!("{:?}", total_count)
 }
